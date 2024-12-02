05c77c716546004f60184de0a50d4cd80e1420b4
dev1d123 <dhuamanio@unsa.edu.pe>
2024-12-02 11:14:47 -0500

    Added adming

--- museum34/Museo/admin.py
+++ museum34/Museo/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Usuario, Modelos, Configuracion, Comentarios, Favorito, Articulos
 
 # Register your models here.
+admin.site.register(Usuario)
+admin.site.register(Modelos)
+admin.site.register(Configuracion)
+admin.site.register(Comentarios)
+admin.site.register(Favorito)
+admin.site.register(Articulos)
